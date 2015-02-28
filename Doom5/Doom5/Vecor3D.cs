9fc34f9a1eaeb1bc4e41d61e5866c18cf26b6202
Arne Rasmussen <Gedesneglen@gmail.com>
2015-03-01 00:37:16 +0100

    Vector

--- Doom5/Doom5/Vecor3D.cs
+++ Doom5/Doom5/Vecor3D.cs
@@ -8,18 +8,36 @@ namespace Doom5
 {
     class Vecor3D
     {
-        double x, z, y;
+        double X, Y, Z;
+
+        public Vecor3D()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+
+        public Vecor3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
 
 
 
 
 
+        public static Vecor3D operator +(Vecor3D a, Vecor3D b)
+        {
 
+            return new Vecor3D((a.X + b.X), (a.Y + b.Y), (a.Z + b.Z));
+        }
 
-        public static Vecor3D operator +(Vecor3D a, Vecor3D b){
+        public static Vecor3D operator -(Vecor3D a, Vecor3D b)
+        {
 
-
-            return null;
+            return new Vecor3D((a.X - b.X), (a.Y - b.Y), (a.Z - b.Z));
         }
     }
 }
