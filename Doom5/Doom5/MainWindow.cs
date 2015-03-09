4f3d19f87155a2a9e13c7afed767bc512eb13351
Arne Rasmussen <Gedesneglen@gmail.com>
2015-03-09 12:40:25 +0100

    yeds
    yes

--- Doom5/Doom5/MainWindow.cs
+++ Doom5/Doom5/MainWindow.cs
@@ -5,16 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+
 namespace Doom5
 {
     class MainWindow : Form
     {
-        Bitmap bmp = new Bitmap(640, 480);
+        Bitmap bmp = new Bitmap(16, 12);
+        Graphics g;
 
         public MainWindow()
         {
-            this.BackgroundImage = bmp;
-            this.BackgroundImageLayout = ImageLayout.Zoom;
+            bmp.SetPixel(1, 1, Color.Black);
+            bmp.SetPixel(1, 11, Color.Black);
+            bmp.SetPixel(7, 7, Color.Black);
+            bmp.SetPixel(15, 11, Color.Black);
+            bmp.SetPixel(15, 1, Color.Black);
+
+            this.SuspendLayout();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            this.CenterToScreen();
+            g = this.CreateGraphics();
+            g.InterpolationMode = InterpolationMode.NearestNeighbor;
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            g.DrawImage(bmp, 0, 0, this.Width, this.Height);
         }
     }
 }
